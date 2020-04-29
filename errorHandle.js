92097b5e716094486867d4824ee0d6bffdf2b906
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-04-29 20:33:50 +0600

    commit

---
+++ errorHandle.js
@@ -0,0 +1,23 @@
+// let info = '{"id": 1,"name": "Leanne Graham","username": "Bret","email": "Sincere@april.biz"}'
+
+// try {
+
+//     let user = JSON.parse(info);
+
+//     console.log(user.name)
+// }
+// catch (e) {
+//     console.log(e.message)
+//     console.log(e.name)
+// }
+
+try {
+    setTimeout(() => {
+        user = "Jamal" // actually we don't define the user
+    }, 1000)
+
+} catch (e) {
+
+    console.log('error block start')
+}
+
